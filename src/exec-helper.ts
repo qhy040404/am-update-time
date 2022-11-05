6a7fa257cba718dca95874d769ccf3c8e53c6d9e
qhy040404 <qhy040404@163.com>
2022-11-05 22:25:48 +0800

    fix push

--- src/exec-helper.ts
+++ src/exec-helper.ts
@@ -1,7 +1,8 @@
 import * as exec from "@actions/exec"
 
 export async function push(token: string, branch:string) {
-    await exec.exec('git commit -am "Update playlist"')
+    await exec.exec('ls')
+    /*await exec.exec('git commit -am "Update playlist"')
     // @ts-ignore
     await exec.exec(`git push ${process.env.GITHUB_SERVER_URL.trim().split("//")[0]}//${process.env.GITHUB_ACTOR}:${token}@${process.env.GITHUB_SERVER_URL.trim().split("//")[1]}/${process.env.GITHUB_REPOSITORY}.git HEAD:${branch}`)
-}
+*/}
