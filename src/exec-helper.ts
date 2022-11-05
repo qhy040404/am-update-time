889aacb2325bbefdc41028e0fe4829188fe92d20
qhy040404 <qhy040404@163.com>
2022-11-05 21:55:48 +0800

    Add push

---
+++ src/exec-helper.ts
@@ -0,0 +1,5 @@
+import * as exec from "@actions/exec"
+
+export async function push(token: string, branch:string) {
+    await exec.exec(`cd .. && git push https://${process.env.GITHUB_ACTOR}:${token}@${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}.git HEAD:${branch}`)
+}
