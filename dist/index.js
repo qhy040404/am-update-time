0ff07427906d210185640fc4111e02d1e78d4a74
qhy040404 <qhy040404@163.com>
2022-11-05 22:28:16 +0800

    fix push

--- dist/index.js
+++ dist/index.js
@@ -43,6 +43,7 @@ exports.push = void 0;
 const exec = __importStar(__nccwpck_require__(1514));
 function push(token, branch) {
     return __awaiter(this, void 0, void 0, function* () {
+        yield exec.exec('git config --local --add safe.directory ${INPUT_DIRECTORY}');
         yield exec.exec('git commit -am "Update playlist"');
         // @ts-ignore
         yield exec.exec(`git push ${process.env.GITHUB_SERVER_URL.trim().split("//")[0]}//${process.env.GITHUB_ACTOR}:${token}@${process.env.GITHUB_SERVER_URL.trim().split("//")[1]}/${process.env.GITHUB_REPOSITORY}.git HEAD:${branch}`);
