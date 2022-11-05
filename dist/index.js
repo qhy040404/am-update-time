814378932128630168c014ce5eadd2d655f3c494
qhy040404 <qhy040404@163.com>
2022-11-05 22:29:40 +0800

    Revert "fix push"
    This reverts commit 0a376525568d1fb473f410e8e445e773ef843f2c.

--- dist/index.js
+++ dist/index.js
@@ -43,8 +43,7 @@ exports.push = void 0;
 const exec = __importStar(__nccwpck_require__(1514));
 function push(token, branch) {
     return __awaiter(this, void 0, void 0, function* () {
-        // @ts-ignore
-        yield exec.exec(`git push ${process.env.GITHUB_SERVER_URL.trim().split("//")[0]}//${process.env.GITHUB_ACTOR}:${token}@${process.env.GITHUB_SERVER_URL.trim().split("//")[1]}/${process.env.GITHUB_REPOSITORY}.git HEAD:${branch}`);
+        yield exec.exec(`git push https://${process.env.GITHUB_ACTOR}:${token}@${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}.git HEAD:${branch}`);
     });
 }
 exports.push = push;
--- src/exec-helper.ts
+++ src/exec-helper.ts
@@ -1,6 +1,5 @@
 import * as exec from "@actions/exec"
 
 export async function push(token: string, branch:string) {
-    // @ts-ignore
-    await exec.exec(`git push ${process.env.GITHUB_SERVER_URL.trim().split("//")[0]}//${process.env.GITHUB_ACTOR}:${token}@${process.env.GITHUB_SERVER_URL.trim().split("//")[1]}/${process.env.GITHUB_REPOSITORY}.git HEAD:${branch}`)
+    await exec.exec(`git push https://${process.env.GITHUB_ACTOR}:${token}@${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}.git HEAD:${branch}`)
 }
